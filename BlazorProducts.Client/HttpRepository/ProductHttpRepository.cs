9683fbd44ab34fe68e1d713ff86c00974e33a0c9
Rafał Świrk <rafalswirk@outlook.com>
2021-08-15 06:00:45 +0200

    - Implementing repository for handling products requests

---
+++ BlazorProducts.Client/HttpRepository/ProductHttpRepository.cs
@@ -0,0 +1,16 @@
+﻿using Entities.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorProducts.Client.HttpRepository
+{
+    public class ProductHttpRepository : IProductHttpRepository
+    {
+        public Task<List<Product>> GetProducts()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
