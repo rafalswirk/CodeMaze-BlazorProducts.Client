615f80ca31530d28615e76cbc5449839d2799bca
Rafał Świrk <rafalswirk@outlook.com>
2021-09-10 09:45:14 +0200

    - Added products page

---
+++ BlazorProducts.Client/Pages/Products.razor.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorProducts.Client.Pages
+{
+    public partial class Products
+    {
+    }
+}
